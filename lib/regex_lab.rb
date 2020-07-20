bc29f13f3f99e8d93b59f1f0a1487086c6f53d0e
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-07-20 17:41:02 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -25,5 +25,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end 
 
 def valid_phone_number?(phone)
-   if text.match()
+   if text.match((/\(?([0-9]{3})\)?([ -]?)([0-9]{3})([ -]?)([0-9]{4})/) == nil)
 end
