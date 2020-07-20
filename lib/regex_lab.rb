9546ad64c935486daa5e519efa93551f0d527d14
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-07-20 17:36:34 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,7 +17,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/^[A-Z].*\.$\W/) == nil 
+  if text.match(/^[A-Z].*\.$/) == nil 
     return false 
   else 
     return true
